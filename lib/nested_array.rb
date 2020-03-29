14843222bf23da8b443a326dcf22f2d5274cf0ad
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-29 21:46:30 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -56,8 +56,9 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
-  matrix[row][column]
-  matrix
+  content = matrix[row][column]
+  content
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
